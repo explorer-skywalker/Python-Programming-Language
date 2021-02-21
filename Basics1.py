3a33a3ad81d71746c6dcb70fad224e3cde837a35
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-02-21 20:56:23 +0530

    Primary Commit

---
+++ Basics1.py
@@ -0,0 +1,78 @@
+
+# repl.it  | with google-acc.
+    # -> glot.io  | can code without signup
+
+
+
+# First program
+name = input('Enter your name; ')
+print('Hello ' + name)
+print('Hello', name)
+
+
+
+# Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Custom Data Types -> class
+
+# Specialized Data Types
+
+None   # noting
+
+
+
+# int & float
+  #-> integer [whole numbers] and floating value [decimal containing numbers]
+  #-> float takes more memory than int
+
+print(3 + 6)
+print(type(3 + 6))
+print(3 - 6)
+print(3 * 6)
+print(3 / 6)
+print(type(3 / 6))
+
+print(3 // 6)   #gives integer always
+print(type(3 // 6))
+
+print(3 % 6)   #remainder
+
+print(3.9 + 6.1)
+print(type(3.9 + 6.1))
+
+
+
+# Math Functions
+
+print(round(3.1))   #near value
+print(round(3.5))
+print(round(3.9))
+
+print(abs(-21))   # net value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
