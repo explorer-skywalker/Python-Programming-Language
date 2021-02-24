446f359ec701a479de41b2a6e53d93366c8a2070
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-02-24 13:21:55 +0530

    Second Commit

--- Basics1.py
+++ Basics1.py
@@ -1,4 +1,6 @@
 
+print('Hello world')
+
 # repl.it  | with google-acc.
     # -> glot.io  | can code without signup
 
@@ -20,11 +22,8 @@ list
 tuple
 set
 dict
-
 # Custom Data Types -> class
-
 # Specialized Data Types
-
 None   # noting
 
 
@@ -32,7 +31,6 @@ None   # noting
 # int & float
   #-> integer [whole numbers] and floating value [decimal containing numbers]
   #-> float takes more memory than int
-
 print(3 + 6)
 print(type(3 + 6))
 print(3 - 6)
@@ -40,10 +38,13 @@ print(3 * 6)
 print(3 / 6)
 print(type(3 / 6))
 
-print(3 // 6)   #gives integer always
+print(3 // 6)        # gives integer always
 print(type(3 // 6))
 
-print(3 % 6)   #remainder
+print(3 % 6)   # remainder
+
+print(9**2)   # Squre of 9
+print(9**3)   # cube of 9
 
 print(3.9 + 6.1)
 print(type(3.9 + 6.1))
@@ -51,20 +52,121 @@ print(type(3.9 + 6.1))
 
 
 # Math Functions
-
-print(round(3.1))   #near value
+print(round(3.1))   # near value
 print(round(3.5))
 print(round(3.9))
 
-print(abs(-21))   # net value
+print(abs(-21))   # absolute value
 
 
 
+'''
+Developer fundamentals ;
+  -> Don't try to learn dictonary
+  -> Programming is solving the problems, don't try to memorise everything
+  -> Take help of google and other things as a reference
+  -> Learn the concepts, they are grammar[OOP] for all programming languages 
+
+'''
 
 
 
+# Operator Precedence
+print((9-3)+30-2**2)
+
+# Calculations follows the BODMAS rule
+  #-> Brackets, Of [Power Of (**)], Division, Multiplcation, Addition & Subtraction 
 
 
+
+# Complex and bin()
+complex
+
+# bin(), binary
+print(5)
+print(bin(5))   # Binary form of integer value, after 0b thats; 101
+print(int('0b101', 2))   
+print(bin(3))
+print(bin(6))
+print(bin(9))
+print(int('0b1001', 2))
+
+
+
+# Variables 
+iq = 199   # variable, must be descripted well
+print(iq)
+
+_iq = 133   # cvariables can start with letters and underscore (_)
+print(_iq)
+
+__iQ = 963   # can do this, but not a good practice
+print(__iQ)
+
+user_name_1 = 'star-wars'  
+print(user_name_1)
+
+User_id1 = 'CaptainMS'
+print(User_id1)
+
+IQ = 166   # variables are case sensitive
+print(IQ)
+
+pc = 'Desktop'
+print(pc)
+pc = 'Laptop'   # Variable's value can be changed
+print(pc)
+
+#constants
+PC = 'Laptop/Desktop'   # Value of this type of variables should not be changed
+print(PC)
+
+a,b,c = 3,6,9   #rapidly assign value to variable
+print(a)
+print(b)
+print(c)
+
+# Keywords must not be assigned as a variable name
+
+
+
+# Expression & Statement
+iq = 199
+user_age = iq // 6   
+# Expression; iq // 6
+# Statement; user_age = iq // 6   
+print(iq)
+print(user_age)
+
+
+
+# Argumented Assignment Operator
+some_value = 3
+some_value = 3 + 6
+print(some_value)
+
+some_value = 3
+some_value = some_value + 6
+print(some_value)
+
+some_value = 3
+some_value += 6   # best way, argumented assignment operator
+print(some_value)
+
+some_value -= 6
+print(some_value)
+
+some_value = 3
+some_value *= 6
+print(some_value)
+
+some_value = 3
+some_value /= 6
+print(some_value)
+
+
+
+# String
 
 
 
