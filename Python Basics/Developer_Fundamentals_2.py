ba885f6b3900062aecb8e933d3b9176bbeef3db2
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-03-02 15:47:25 +0530

    Primary Commit

---
+++ Python Basics/Developer_Fundamentals_2.py
@@ -0,0 +1,58 @@
+
+# Commenting the code;
+  #-> Comment the code for understanding & for future reference
+  #-> Ctrl + /   ; To comment a whole line
+
+print('hello world')   # Comment after code
+
+'Refered as string comment'
+"Refered as string comment"
+
+'''
+Refered
+as
+string
+comment
+
+'''
+
+"""
+Refered 
+as
+string
+comment
+
+ WOW
+ 0 0
+  ^
+ ---
+
+"""
+
+# docstring?
+
+
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
