949952ce5beb731482e2b401aa748f5d037a62a0
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-05-11 15:03:26 +0530

    Primary Commit

---
+++ Test-1.py
@@ -0,0 +1,30 @@
+
+# Test File
+
+
+# 1
+a = b = c = "orange"
+
+print(a)
+
+print(b)
+
+print(c)
+
+
+# 2
+x = "hello"[0]
+print(x)
+
+
+# 3
+f = 5
+print(type(f))
+
+
+# 4
+fruits = ['banana', "apple", "orange"]
+print(fruits[-1])
+
+
+# 5
---
+++ Web Development With Python/Basics.py
@@ -0,0 +1,8 @@
+
+"""
+Web Development With Python;
+
+    -> 
+
+
+"""
