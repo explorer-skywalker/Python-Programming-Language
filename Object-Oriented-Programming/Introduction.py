24229042b2c51be83ca324e6525f78c00027ab3f
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-05-24 02:29:30 +0530

    Primary Commit

--- Object-Oriented-Programming/Introduction.py
+++ Object-Oriented-Programming/Introduction.py
@@ -2,9 +2,24 @@
 """
 What is Object Oriented Programming (OOP);
 
-    -> 
+    -> A way to structure & manage a large code base. 
 
 
 """
+
+print(type(None))
+print(type(True))
+print(type(False))
+print(type('hello world'))
+print(type(369))
+print(type(33.69))
+print(type(()))
+print(type([]))
+print(type({}))
+
+
+
+
+
 
 
