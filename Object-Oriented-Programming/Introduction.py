a9921f1590d058a25bcbe39493000dba80713569
Captain Murlidhar Singh <murlidharsingh1319@gmail.com>
2021-05-28 23:59:43 +0530

    OOP

--- Object-Oriented-Programming/Introduction.py
+++ Object-Oriented-Programming/Introduction.py
@@ -2,11 +2,17 @@
 """
 What is Object Oriented Programming (OOP);
 
-    -> A way to structure & manage a large code base. 
+    -> A way to structure & manage a large code base without messing with a lot of code, just create a simple blueprint...
+
+    -> Blueprint - Instanciate - Instances_
+
+    -> OOP keeps code Reusable, Organised & Memory Efficient.
 
 
 """
 
+
+# Class, blueprints
 print(type(None))
 print(type(True))
 print(type(False))
@@ -19,6 +25,22 @@ print(type({}))
 
 
 
+# Custom Class, blueprint
+class BigObject:
+    pass
+print(type(BigObject))
+
+object1 = BigObject()   # Instance, Creating an object by the blueprint defined above...
+print(type(object1))
+
+object2 = BigObject()   # Instanciate, Instance 
+
+
+
+
+
+
+print('\n\n\n')
 
 
 
