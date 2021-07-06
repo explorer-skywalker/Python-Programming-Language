f0daf3a9e0e0ef12c1693113a9ef54031a653970
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-07-06 11:24:30 +0530

    OOP

---
+++ OOP/OOP-test-1.0.py
@@ -0,0 +1,13 @@
+
+class Test:
+    def __init__(self, name="Human", skills="No Data :(", planet="Earth") -> None:
+        self.name = name 
+        self.skills = skills
+        self.planet = planet
+
+
+whoami = Test("Captain Skywalker", "Explorer (Researcher, Developer & Hacker")
+print(whoami.name)
+print(whoami.skills)
+print(whoami.planet)
+
