a4ae93f29e57b5b75a9195a0bd3933ef54b051c0
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-07-28 20:26:24 +0530

    OOP

--- 6_OOP/2-Abstraction.py
+++ 6_OOP/2-Abstraction.py
@@ -1,18 +1,49 @@
 
 """
 Abstraction:
-                Extraction of data, as we wanted & hide all other stuff.
+            >_ Extraction of data, as we wanted & hide all other stuff.
 
 """
 
 
+class Universe():
+    def __init__(self, forces="""Electromagnetism", "Gravity", "Strong & Weak Nuclear Forces""", laws="Laws Of Physics") -> None:
+        self.forces = forces
+        self.laws = laws
+
+
+    def life(self):
+        return self.forces + " And " + self.laws
+
+
+earth = Universe()
+print("Fundamental Forces: \n", earth.forces)
+print(f"Laws of Earth means: \n {earth.laws}")
+
+earth.laws = "made by the peoples who forms the government ;)"
+print("Today's citizen Rules are", earth.laws)
+
+
+human = Universe("Unknown", "Nonsence")
+print(f"Human forces are {human.forces} & human laws are {human.laws}")
 
 
 
 
+"""
+Private vs Public Variables:
+                            >_ There is no such functions as private but, the python community decided that ( _ ) started variables should be private & not modified.
+
+"""
+
+class Dark():
+    def __init__(self, matter="Dark Matter", energy="Dark Energy") -> None:
+        self._matter = matter
+        self._energy = energy
 
 
-
-
-
+research = Dark()
+print(research._matter)
+print(research._energy)
+print(f"This research topic about {research._matter} and {research._energy} are still under R&D...")
 
