ecec563102d7ded7b1d2a08ff001f15b7dc9412c
Captain Skywalker <70905670+skywalker1319@users.noreply.github.com>
2021-08-03 23:34:49 +0530

    Updated

--- 6_OOP/3-Inheritance.py
+++ 6_OOP/3-Inheritance.py
@@ -1,4 +1,35 @@
 
+class Human():
+    def identification(self):
+        print("Welcome Dreamers, Explorers & Creators !")
+
+
+# Inheritances_
+
+class Developers(Human):
+    pass
+
+
+class Hackers(Human):
+    pass
+
+
+class Programmers(Human):
+    pass
+
+
+class Engineers(Human):
+    pass
+
+
+class Scientists(Human):
+    pass
+
+
+
+human1 = Developers()
+print(human1.identification())
+
 
 
 
