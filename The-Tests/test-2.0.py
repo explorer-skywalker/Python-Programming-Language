c12e7ac619c9b3d07c5f595c1baa0501455cea29
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-08-30 18:40:54 +0530

    Tests

--- The-Tests/test-2.0.py
+++ The-Tests/test-2.0.py
@@ -5,28 +5,28 @@ Purpose: Testing an Idea
 Stardate: 12021.242.01:21:00
 """
 
+
 class Human():
-    def __init__(self, mindset="\nScientist\n", nature="Creator\n" , work="Explorer\n", thinking="Dreamer\n"):
+    def __init__(self, mindset="\nScientist\n", nature="Creator\n", work="Explorer\n", thinking="Dreamer\n"):
         self.mindset = mindset
         self.nature = nature
         self.work = work
         self.thinking = thinking
 
-
     def Intro(self):
-        return "Hello, I'm Captain Skywalker. I'm a Scientist & my nature is Research, Explore and Develop.. I'm the founder & C.E.O of ASAI Inc. (Automation Systems with Artificial Intelligence)... My ultimate aim is to develop a Galactic Human Empire....\n "
-
+        return ">_  Hello, I'm Captain Skywalker. I'm a Scientist & my nature is Research, Explore and Develop.. I'm the founder & C.E.O of ASAI Inc. (Automation Systems with Artificial Intelligence)... My ultimate aim is to develop a Galactic Human Empire....\n "
 
     def Logic(self):
-        return "At the ultimate level, Everything Is Nothing because, this very universe itself doesn't have any purpose, It just is...\n"
-
+        return ">_  At the ultimate level, Everything Is Nothing because, this very universe itself doesn't have any purpose, It just is...\n"
 
     def Ultimate_Aim(self):
-        return "If Everything Is Nothing then, You have to do the greatest thing that you ever could & Which is developing a Galactic Human Empire for good, advancement and to improve the quality of life...\n"
-
+        return ">_  If Everything Is Nothing then, You have to do the greatest thing that you ever could & Which is developing a Galactic Human Empire for good, advancement and to improve the quality of life...\n"
 
     def Perfect(self):
-        return "There is nothing like perfect, It just the matter of the perseption of reality, Just be and build something better than ever that, You can proud of...\n"
+        return ">_  There is nothing like perfect, It just the matter of the perseption of reality, Just be and build something better than ever that, You can proud of...\n"
+
+    def Possibility(self):
+        return "\n\t>_  Everything Is Possible through Technological Advancements...\n"
 
 
 skywalker = Human()
@@ -35,7 +35,9 @@ print(skywalker.nature)
 print(skywalker.work)
 print(skywalker.thinking)
 
-print(skywalker.Intro())
-print(skywalker.Logic())
-print(skywalker.Perfect())
-print(skywalker.Ultimate_Aim())
+ultimate = Human()
+print(ultimate.Intro())
+print(ultimate.Logic())
+print(ultimate.Perfect())
+print(ultimate.Ultimate_Aim())
+print(ultimate.Possibility())
