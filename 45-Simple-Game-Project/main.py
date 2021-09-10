1a229a19499ee6b07a61355a391ef893f719baee
Skywalker <70905670+skywalker1319@users.noreply.github.com>
2021-09-11 00:58:56 +0530

    Game-Project

---
+++ 45-Simple-Game-Project/main.py
@@ -0,0 +1,28 @@
+"""
+    Developer: Master Skywalker
+    Purpose: Game Development Project with C++
+    Stardate: 12021.254.00:45:32
+
+"""
+
+print("""
+            "ASAI's Research Lab Is Under Attack, Break The Codes To Get Into The Datacentre Facility, Cut The Hard Line & Save The Research Data From Going Into Wrong Hands..."
+            
+            """)
+
+num1 = 3
+num2 = 6
+num3 = 9
+
+sum = num1 + num2 + num3
+product = num1 * num2 * num3
+
+print(">_\tHere's Some Info...")
+print("+ The Numbers Of Code: 3")
+print("+ The Codes Join Upto: ", sum)
+print("+ The Codes Product Upto: ", product)
+
+userCode = int(input("\n\tNow, Enter The Codes: ").strip())
+
+
+
